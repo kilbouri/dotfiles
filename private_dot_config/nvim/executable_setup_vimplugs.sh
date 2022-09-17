02d0e1e3203507e9727defa98187d4539a90b3bb
Isaac Kilbourne <kilbouri@uwindsor.ca>
2022-09-17 16:45:50 -0400

    Add VimPlugs setup script for NeoVim

---
+++ private_dot_config/nvim/executable_setup_vimplugs.sh
@@ -0,0 +1,6 @@
+echo "Installing Vim Plugs for NeoVim"
+
+sh -c 'curl -sfLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+  --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "Finished!"
